5bffa84b39b772058bfba32e33aaedd46aaf691a
Kravchuk <majerkravchuk@gmail.com>
2018-06-03 22:39:48 +0300

    Seeds for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-if Rails.env.development?
+# if Rails.env.development?
   business = Business.create(name: 'DMS/Envision', subdomain: 'dms')
   fake = Business.create(name: 'Fake business', subdomain: 'fake')
 
@@ -47,4 +47,4 @@ if Rails.env.development?
     business: business,
     position: positions.second
   )
-end
+# end
