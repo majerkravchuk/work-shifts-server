618675c903a6bea9c8e4dde4555027ee2de0abf8
Kravchuk <majerkravchuk@gmail.com>
2018-07-04 16:46:16 +0300

    Fix views for emails

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -3,12 +3,8 @@ class InvitationMailer < ApplicationMailer
     @invitation = invitation
     @manager = invitation.manager
     @business = invitation.business
+    @template = ERB.new(EmailTemplate.email_template_for('invitation').body).result(binding).html_safe
 
-    mail(
-      to: @invitation.email,
-      subject: 'Invitation to join',
-      body: ERB.new(EmailTemplate.email_template_for('invitation').body).result(binding).html_safe,
-      content_type: 'text/html'
-    )
+    mail(to: @invitation.email, subject: 'Invitation to join')
   end
 end
