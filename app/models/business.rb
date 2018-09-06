058283bcb9fdcb0c57b03b933f9a80fe6f769fc9
Kravchuk <majerkravchuk@gmail.com>
2018-09-06 20:13:59 +0300

    Fix relations between business and positions

--- app/models/business.rb
+++ app/models/business.rb
@@ -19,8 +19,8 @@ class Business < ApplicationRecord
   has_many :employees
   has_many :facilities
   has_many :positions
-  # has_many :manager_positions
-  # has_many :employee_positions
+  has_many :manager_positions, -> { where(role: :manager) }, class_name: 'Position'
+  has_many :employee_positions, -> { where(role: :employee) }, class_name: 'Position'
   has_many :shifts
   has_many :email_loader_results, class_name: 'EmailLoader::Result'
   has_many :audits, class_name: 'BusinessAudit'
