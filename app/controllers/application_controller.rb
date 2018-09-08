53a0012b6ecf6e1ff90d4a8f79b58ae6409a8d7c
Kravchuk <majerkravchuk@gmail.com>
2018-09-09 00:19:34 +0300

    Init gon variables for the switching menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :check_current_user_business
   before_action :current_business
   before_action :set_time_zone
+  before_action :set_gon_available_businesses
 
   def check_current_user_business
     current_user.update(business: Business.first) if current_user.present? && current_user.business.nil?
@@ -14,6 +15,12 @@ class ApplicationController < ActionController::Base
     @current_business ||= current_user.business
   end
   helper_method :current_business
+
+  def set_gon_available_businesses
+    return unless is_a?(ActiveAdmin::BaseController)
+    businesses = current_user.admin? ? Business.all : current_user.businesses
+    gon.availableBusinesses = businesses.map { |business| { id: business.id, name: business.name } }
+  end
 
   def set_time_zone
     Time.zone = current_business.time_zone if current_business.present?
