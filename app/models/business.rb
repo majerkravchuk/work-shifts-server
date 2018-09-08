d0e52d32435c2f5499ab3e1f8c39f7afa49cb3ea
Kravchuk <majerkravchuk@gmail.com>
2018-09-08 20:17:37 +0300

    Update relations in the business model

--- app/models/business.rb
+++ app/models/business.rb
@@ -15,12 +15,16 @@ class Business < ApplicationRecord
 
   # === relations ===
   has_many :users
-  has_many :managers
-  has_many :employees
+  has_many :profiles
+  has_many :workers, through: :profiles, source: :user
+  has_many :employee_profiles, class_name: 'Profile::Employee'
+  has_many :manager_profiles, class_name: 'Profile::Manager'
+  has_many :employees, through: :employee_profiles, source: :user
+  has_many :managers, through: :manager_profiles, source: :user
+  has_many :employee_positions, class_name: 'Position::Employee'
+  has_many :manager_positions, class_name: 'Position::Manager'
   has_many :facilities
   has_many :positions
-  has_many :manager_positions, -> { where(role: :manager) }, class_name: 'Position'
-  has_many :employee_positions, -> { where(role: :employee) }, class_name: 'Position'
   has_many :shifts
   has_many :email_loader_results, class_name: 'EmailLoader::Result'
   has_many :audits, class_name: 'BusinessAudit'
