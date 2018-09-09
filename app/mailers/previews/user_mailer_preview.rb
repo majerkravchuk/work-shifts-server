7ca5f259f49c7849842fd174b2ad9653983a86fe
Kravchuk <majerkravchuk@gmail.com>
2018-09-09 23:53:33 +0300

    Fix the template rendering for the invitations

--- app/mailers/previews/user_mailer_preview.rb
+++ app/mailers/previews/user_mailer_preview.rb
@@ -1,5 +1,5 @@
-class InvitationMailerPreview < ActionMailer::Preview
+class UserMailerPreview < ActionMailer::Preview
   def invitation
-    InvitationMailer.invitation(Invitation.first)
+    UserMailer.invitation(User.first)
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,10 @@
-class InvitationMailer < ApplicationMailer
+class UserMailer < ApplicationMailer
   def invitation(user)
     @user = user
     @manager = user.inviter
     @business = user.business
     @token = user.invitation_token
-    @template = ERB.new(EmailTemplate.email_template_for('invitation').body).result(binding).html_safe
 
-    mail(to: @invitation.email, subject: 'Invitation to join')
+    mail(to: @user.email, subject: 'Invitation to join')
   end
 end
