d90551a874738f50c2850eb0110d2f60eb09520d
Kravchuk <majerkravchuk@gmail.com>
2018-09-09 23:52:29 +0300

    Renam InvitationMailer to UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class InvitationMailer < ApplicationMailer
-  def invitation(invitation)
-    @invitation = invitation
-    @manager = invitation.manager
-    @business = invitation.business
+  def invitation(user)
+    @user = user
+    @manager = user.inviter
+    @business = user.business
+    @token = user.invitation_token
     @template = ERB.new(EmailTemplate.email_template_for('invitation').body).result(binding).html_safe
 
     mail(to: @invitation.email, subject: 'Invitation to join')
