2b1a400bd6a6652e7005468d6cdfa2f4f4382c2b
Kravchuk <majerkravchuk@gmail.com>
2018-09-09 13:44:22 +0300

    Fix worker seeds

--- db/seeds/worker.rb
+++ db/seeds/worker.rb
@@ -3,9 +3,17 @@ require "#{Rails.root}/db/seeds/base"
 module Seeds
   class Worker < Base
     def seed!
-      20.times do |i|
-        user = ::User::Worker.create(email: "user-#{i + 1}@example.com", name: "User #{i + 1}")
-        log(user)
+      ::Business.all.each do |business|
+        positions = business.positions.all.to_a
+        10.times do |i|
+          user = ::User::Worker.create(
+            email: "user-#{i + 1}-b-#{business.id}@example.com",
+            name: "User #{i + 1} for business #{business.id}",
+            business: business,
+            position: positions.sample
+          )
+          log(user)
+        end
       end
     end
   end
