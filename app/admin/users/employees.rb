cfd12609ff3326be23bce4910f989acbcf69d2e1
Kravchuk <majerkravchuk@gmail.com>
2018-09-09 13:31:39 +0300

    Fix the admin panel for employee and managers

--- app/admin/users/employees.rb
+++ app/admin/users/employees.rb
@@ -1,26 +1,29 @@
 ActiveAdmin.register User::Worker, as: 'Employee' do
-  menu parent: 'Users', priority: 3
+  menu parent: 'Users', priority: 4
 
-  includes profiles: :position
+  includes :position
 
-  # permit_params :name, :email, :business_id, :position_id, :password, :password_confirmation, facility_ids: []
+  permit_params :name, :email, :business_id, :position_id, :password, :password_confirmation, facility_ids: []
 
   config.sort_order = 'name_asc'
 
   controller do
     def scoped_collection
-      end_of_association_chain.joins(profiles: :position)
-                              .where(profiles: { positions: { type: 'Position::Employee' } })
-                              .distinct
+      end_of_association_chain.joins(:position).where(positions: { type: 'Position::Employee' }).distinct
+    end
+
+    def create
+      params[:user_worker][:business_id] = current_business.id
+      create! { admin_employees_path }
     end
 
     def update
-      if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
-        params[:user].delete('password')
-        params[:user].delete('password_confirmation')
+      if params[:user_worker][:password].blank? && params[:user_worker][:password_confirmation].blank?
+        params[:user_worker].delete('password')
+        params[:user_worker].delete('password_confirmation')
       end
       update! do |format|
-        format.html { redirect_to send("admin_#{resource.role}_path", resource) }
+        format.html { redirect_to send("admin_#{resource.role.pluralize}_path") }
       end
     end
   end
@@ -32,21 +35,15 @@ ActiveAdmin.register User::Worker, as: 'Employee' do
 
   filter :name
   filter :email
-  # filter :position,
-  #        as: :select,
-  #        collection: proc { current_business.employee_positions }
-  # filter :facilities,
-  #        as: :select,
-  #        collection: proc { current_business.facilities }
-  # filter :created_at
+  filter :position, as: :select, collection: proc { current_business.employee_positions }
+  filter :facilities, as: :select, collection: proc { current_business.facilities }
+  filter :created_at
 
   index do
     column :name
     column :email
-    column(:position) { |resource| resource.profile(current_business).position.name.capitalize }
-    # column(:facilities) do |employee|
-    #   employee.facilities.pluck(:name).sort.join(', ')
-    # end
+    column(:position) { |resource| resource.position.name.capitalize }
+    column(:facilities) { |resource| resource.facilities.pluck(:name).sort.join(', ') }
     column :created_at
     actions defaults: true do |resource|
       link_to 'Reset password', reset_password_admin_employee_path(resource), method: :post
@@ -57,37 +54,35 @@ ActiveAdmin.register User::Worker, as: 'Employee' do
     attributes_table do
       row :name
       row :email
-      row(:position) { |user| user.profile(current_business).position.name.capitalize }
-      # row(:facilities) { |employee| employee.facilities.pluck(:name).sort.join(', ') }
+      row(:position) { |resource| resource.position.name.capitalize }
+      row(:facilities) { |resource| resource.facilities.pluck(:name).sort.join(', ') }
       row :created_at
     end
   end
-  #
-  # form do |f|
-  #   form = ActiveAdmin::UserFormGenerator.new(current_user, current_business, f.object)
-  #   form.generate_form_proc.call(f)
-  # end
-  #
-  # # form do |f|
-  # #   f.inputs do
-  # #     f.input :name
-  # #     f.input :email
-  # #     f.input :password
-  # #     f.input :password_confirmation
-  # #     # f.input(:position, {
-  # #     #   as: :select,
-  # #     #   collection: current_user.employee_positions.map { |p| [p.name.capitalize, p.id] },
-  # #     #   include_blank: false
-  # #     # })
-  # #     collected_data = current_business.facilities.map do |facility|
-  # #       [
-  # #         facility.name,
-  # #         facility.id,
-  # #         { checked: f.object.facilities.include?(facility) }
-  # #       ]
-  # #     end
-  # #     f.input :facilities, as: :check_boxes, collection: collected_data
-  # #   end
-  # #   f.actions
-  # # end
+
+  form do |f|
+    #   generator = ActiveAdmin::UserFormGenerator.new(current_user, current_business, f.object)
+    #   form_proc = generator.generate_form_proc
+    #   form_proc.call(f)
+
+    f.inputs do
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :position,
+              as: :select,
+              collection: current_business.employee_positions.order(:name).map { |p| [p.name, p.id] },
+              include_blank: false
+      collected_data = current_business.facilities.map do |facility|
+        [
+          facility.name,
+          facility.id,
+          { checked: f.object.facilities.include?(facility) }
+        ]
+      end
+      f.input :facilities, as: :check_boxes, collection: collected_data
+    end
+    f.actions
+  end
 end
