e27047eb1e3e0fb75dccaadb57d467c5df7b50f6
Kravchuk <majerkravchuk@gmail.com>
2018-09-09 10:18:28 +0300

    Fix index and show pages on the admin panel for employee and managers

--- app/admin/users/employees.rb
+++ app/admin/users/employees.rb
@@ -1,24 +1,27 @@
-ActiveAdmin.register Employee do
+ActiveAdmin.register User::Worker, as: 'Employee' do
   menu parent: 'Users', priority: 3
 
-  includes :position, :facilities
+  includes profiles: :position
 
-  permit_params :name, :email, :business_id, :position_id, :password, :password_confirmation, facility_ids: []
+  # permit_params :name, :email, :business_id, :position_id, :password, :password_confirmation, facility_ids: []
 
   config.sort_order = 'name_asc'
 
   controller do
-    def create
-      params[:employee][:business_id] = current_business.id
-      super
+    def scoped_collection
+      end_of_association_chain.joins(profiles: :position)
+                              .where(profiles: { positions: { type: 'Position::Employee' } })
+                              .distinct
     end
 
     def update
-      if params[:employee][:password].blank? && params[:employee][:password_confirmation].blank?
-        params[:employee].delete('password')
-        params[:employee].delete('password_confirmation')
+      if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+        params[:user].delete('password')
+        params[:user].delete('password_confirmation')
       end
-      super
+      update! do |format|
+        format.html { redirect_to send("admin_#{resource.role}_path", resource) }
+      end
     end
   end
 
@@ -29,24 +32,24 @@ ActiveAdmin.register Employee do
 
   filter :name
   filter :email
-  filter :position,
-         as: :select,
-         collection: proc { current_business.employee_positions }
-  filter :facilities,
-         as: :select,
-         collection: proc { current_business.facilities }
-  filter :created_at
+  # filter :position,
+  #        as: :select,
+  #        collection: proc { current_business.employee_positions }
+  # filter :facilities,
+  #        as: :select,
+  #        collection: proc { current_business.facilities }
+  # filter :created_at
 
   index do
     column :name
     column :email
-    column(:position) { |user| user.position.name.capitalize }
-    column(:facilities) do |employee|
-      employee.facilities.pluck(:name).sort.join(', ')
-    end
+    column(:position) { |resource| resource.profile(current_business).position.name.capitalize }
+    # column(:facilities) do |employee|
+    #   employee.facilities.pluck(:name).sort.join(', ')
+    # end
     column :created_at
-    actions defaults: true do |manager|
-      link_to 'Reset password', reset_password_admin_employee_path(manager), method: :post
+    actions defaults: true do |resource|
+      link_to 'Reset password', reset_password_admin_employee_path(resource), method: :post
     end
   end
 
@@ -54,32 +57,37 @@ ActiveAdmin.register Employee do
     attributes_table do
       row :name
       row :email
-      row(:position) { |employee| employee.position.name.capitalize }
-      row(:facilities) { |employee| employee.facilities.pluck(:name).sort.join(', ') }
+      row(:position) { |user| user.profile(current_business).position.name.capitalize }
+      # row(:facilities) { |employee| employee.facilities.pluck(:name).sort.join(', ') }
       row :created_at
     end
   end
-
-  form do |f|
-    f.inputs do
-      f.input :name
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
-      f.input(:position, {
-        as: :select,
-        collection: current_user.employee_positions.map { |p| [p.name.capitalize, p.id] },
-        include_blank: false
-      })
-      collected_data = current_business.facilities.map do |facility|
-        [
-          facility.name,
-          facility.id,
-          { checked: f.object.facilities.include?(facility) }
-        ]
-      end
-      f.input :facilities, as: :check_boxes, collection: collected_data
-    end
-    f.actions
-  end
+  #
+  # form do |f|
+  #   form = ActiveAdmin::UserFormGenerator.new(current_user, current_business, f.object)
+  #   form.generate_form_proc.call(f)
+  # end
+  #
+  # # form do |f|
+  # #   f.inputs do
+  # #     f.input :name
+  # #     f.input :email
+  # #     f.input :password
+  # #     f.input :password_confirmation
+  # #     # f.input(:position, {
+  # #     #   as: :select,
+  # #     #   collection: current_user.employee_positions.map { |p| [p.name.capitalize, p.id] },
+  # #     #   include_blank: false
+  # #     # })
+  # #     collected_data = current_business.facilities.map do |facility|
+  # #       [
+  # #         facility.name,
+  # #         facility.id,
+  # #         { checked: f.object.facilities.include?(facility) }
+  # #       ]
+  # #     end
+  # #     f.input :facilities, as: :check_boxes, collection: collected_data
+  # #   end
+  # #   f.actions
+  # # end
 end
--- app/admin/users/managers.rb
+++ app/admin/users/managers.rb
@@ -1,70 +1,105 @@
-ActiveAdmin.register Manager do
-  menu parent: 'Users', priority: 2, if: -> { current_user.administrator? }
+ActiveAdmin.register User::Worker, as: 'Manager' do
+  menu parent: 'Users', priority: 2, if: -> { current_user.admin? }
 
-  includes :position
+  includes profiles: :position
 
-  permit_params :name, :email, :business_id, :position_id, :password, :password_confirmation
+  # permit_params :name, :email, :business_id, :position_id, :role, :password, :password_confirmation
 
   config.sort_order = 'name_asc'
 
   controller do
-    def create
-      params[:manager][:business_id] = current_business.id
-      super
+    def scoped_collection
+      end_of_association_chain.joins(profiles: :position)
+                              .where(profiles: { positions: { type: 'Position::Manager' } })
+                              .distinct
     end
-
-    def update
-      if params[:manager][:password].blank? && params[:manager][:password_confirmation].blank?
-        params[:manager].delete('password')
-        params[:manager].delete('password_confirmation')
-      end
-      super
-    end
+#
+#     def create
+#       params[:user][:business_id] = current_business.id
+#       super
+#     end
+#
+#     def update
+#       if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+#         params[:user].delete('password')
+#         params[:user].delete('password_confirmation')
+#       end
+#       update! do |format|
+#         format.html { redirect_to send("admin_#{resource.role}_path", resource) }
+#       end
+#     end
   end
-
-  member_action :reset_password, method: :post do
-    resource.send_reset_password_instructions
-    redirect_to admin_managers_path, notice: 'The instruction for password recovery was successfully sent.'
-  end
-
-  filter :name
-  filter :email
-  filter :position,
-         as: :select,
-         collection: proc { current_business.manager_positions }
-  filter :created_at
+#
+#   member_action :reset_password, method: :post do
+#     resource.send_reset_password_instructions
+#     redirect_to admin_managers_path, notice: 'The instruction for password recovery was successfully sent.'
+#   end
+#
+#   filter :name
+#   filter :email
+#   filter :position,
+#          as: :select,
+#          collection: proc { current_business.manager_positions }
+#   filter :created_at
 
   index do
     column :name
     column :email
-    column(:position) { |user| user.position.name.capitalize }
+    column(:position) { |resource| resource.profile(current_business).position.name.capitalize }
     column :created_at
-    actions defaults: true do |manager|
-      link_to 'Reset password', reset_password_admin_manager_path(manager), method: :post
+    actions defaults: true do |resource|
+      link_to 'Reset password', reset_password_admin_employee_path(resource), method: :post
     end
   end
 
+#   index do
+#     column :name
+#     column :email
+#     column(:position) { |user| user.position.name.capitalize }
+#     column :created_at
+#     actions defaults: true do |manager|
+#       link_to 'Reset password', reset_password_admin_manager_path(manager), method: :post
+#     end
+#   end
+#
   show do
     attributes_table do
       row :name
       row :email
-      row(:position) { |user| user.position.name.capitalize }
+      row(:position) { |resource| resource.profile(current_business).position.name.capitalize }
       row :created_at
     end
   end
-
-  form do |f|
-    f.inputs do
-      f.input :name
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
-      # f.input(:position, {
-      #   as: :select,
-      #   collection: ManagerPosition.where(business: current_business).map { |p| [p.name.capitalize, p.id] },
-      #   include_blank: false
-      # })
-    end
-    f.actions
-  end
+#
+#   # form do |f|
+#   #   generator = ActiveAdmin::UserFormGenerator.new(current_user, current_business, f.object)
+#   #   form_proc = generator.generate_form_proc
+#   #   form_proc.call(f)
+#   # end
+#
+#   #   f.inputs do
+#   #     f.input :name
+#   #     f.input :email
+#   #     f.input :password
+#   #     f.input :password_confirmation
+#   #     f.input(:role, {
+#   #       collection: { Employee: 'employee', Manager: 'manager' },
+#   #       include_blank: false
+#   #     })
+#   #     f.input(:position, {
+#   #       as: :select,
+#   #       collection: current_business.manager_positions.order(:name).map { |p| [p.name, p.id] },
+#   #       include_blank: false
+#   #     })
+#   #     collected_data = current_business.facilities.map do |facility|
+#   #       [
+#   #         facility.name,
+#   #         facility.id,
+#   #         { checked: f.object.facilities.include?(facility) }
+#   #       ]
+#   #     end
+#   #     f.input :facilities, as: :check_boxes, collection: collected_data
+#   #   end
+#   #   f.actions
+#   # end
 end
