3f945beb44759fcc4e0b46b4bfceddd05c734a14
Kravchuk <majerkravchuk@gmail.com>
2018-09-09 11:03:34 +0300

    Create the policy for the super admin

---
+++ app/policies/user/super_admin_policy.rb
@@ -0,0 +1,48 @@
+class User
+  class SuperAdminPolicy < ApplicationPolicy
+    def index?
+      current_user.super_admin?
+    end
+
+    def show?
+      current_user.super_admin?
+    end
+
+    def create?
+      current_user.super_admin?
+    end
+
+    def new?
+      current_user.super_admin?
+    end
+
+    def edit?
+      current_user.super_admin?
+    end
+
+    def destroy?
+      current_user.super_admin?
+    end
+
+    def invite?
+      current_user.super_admin?
+    end
+
+    def reset_password?
+      current_user.super_admin?
+    end
+
+    class Scope
+      attr_reader :current_user, :scope
+
+      def initialize(current_user, scope)
+        @current_user = current_user
+        @scope = scope
+      end
+
+      def resolve
+        current_user.super_admin? ? @scope : []
+      end
+    end
+  end
+end
