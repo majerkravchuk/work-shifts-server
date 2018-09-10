09aeced0214f3143dba4cc82fc886b3a1a97b1d2
Kravchuk <majerkravchuk@gmail.com>
2018-09-10 20:21:44 +0300

    Add the method for api client errors renderig

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,4 +6,12 @@ class ApiController < ApplicationController
   def authenticate
     render json: { error: 'Unauthorized' }, status: 401 unless current_user.present?
   end
+
+  def render_client_errors(errors, status = :bad_request)
+    errors = [errors] unless errors.is_a?(Array)
+    render json: {
+      success: false,
+      errors: errors
+    }, status: status
+  end
 end
