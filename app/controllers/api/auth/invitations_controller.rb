a68d8e498db6a1924aa921df096cdfec7133f6f5
Kravchuk <majerkravchuk@gmail.com>
2018-09-11 22:23:21 +0300

    Implement the create action in the InvitationController

--- app/controllers/api/auth/invitations_controller.rb
+++ app/controllers/api/auth/invitations_controller.rb
@@ -1,27 +1,53 @@
 module Api
   module Auth
     class InvitationsController < ApiController
+      include Devise::Controllers::SignInOut
+
+      before_action :find_user_by_invitation_token, only: :show
+      before_action :find_user_by_email, only: :create
+      before_action :validate_invitation_status
+
       skip_before_action :authenticate
 
       # GET /api/auth/invitation?token=token...
       def show
-        user = User.find_by(invitation_token: params[:token])
+        render json: UserSerializer.new(@user)
+      end
 
-        if user.nil?
-          render_client_errors('User with this invitation token does not exist.', :not_found)
+      # POST /api/auth/invitation
+      def create
+        @user.assign_attributes user_params.merge({ invitation_status: :accepted })
 
-        elsif user.uploaded?
-          render_client_errors('User is uploaded to the system, but not yet invited.', :unprocessable_entity)
-
-        elsif user.accepted?
-          render_client_errors('User has already accepted the invitation.', :unprocessable_entity)
-
-        elsif user.invited?
-          render json: UserSerializer.new(user)
-
+        if @user.save
+          sign_in(:user, @user)
+          render json: CurrentUserSerializer.new(@user, include: [:business])
         else
-          render_client_errors('Something went wrong. Please contact your administrator.')
+          render_client_errors(@user.errors.messages.values.flatten, :unprocessable_entity)
         end
+      end
+
+      private
+
+      def find_user_by_invitation_token
+        @user = User.find_by invitation_token: params[:token]
+        render_client_errors('User with this invitation token does not exist', :not_found) if @user.nil?
+      end
+
+      def find_user_by_email
+        @user = User.find_by email: user_params[:email]
+        render_client_errors('User with this email does not exist', :not_found) if @user.nil?
+      end
+
+      def validate_invitation_status
+        if @user.uploaded?
+          render_client_errors('User is uploaded to the system, but not yet invited', :unprocessable_entity)
+        elsif @user.accepted?
+          render_client_errors('User has already accepted the invitation', :unprocessable_entity)
+        end
+      end
+
+      def user_params
+        params.require(:user).permit(:email, :name, :password, :password_confirmation)
       end
     end
   end
