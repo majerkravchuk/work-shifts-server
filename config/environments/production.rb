94e18e5af9af2d6f9b0f38070f793ea6b68579de
Kravchuk <majerkravchuk@gmail.com>
2018-09-11 23:34:40 +0300

    Fix the port in default url options in the production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'work-shifts.ru', port: '80', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'work-shifts.ru', port: 443, protocol: 'https' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
