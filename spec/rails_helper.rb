853a752d29f963a905882bb1b465771639555491
Kravchuk <majerkravchuk@gmail.com>
2018-09-15 20:13:27 +0300

    Fix the RSpec configuration with the FactoryBot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'support/factory_bot'
 
 SimpleCov.start 'rails'
 
