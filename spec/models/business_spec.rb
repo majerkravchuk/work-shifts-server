63f65e907ebcc0637eefd12034ff2c366301ab49
Kravchuk <majerkravchuk@gmail.com>
2018-09-15 20:33:55 +0300

    Fix business_spec annotations

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: businesses
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string
+#  time_zone  :string           default("Pacific Time (US & Canada)")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 RSpec.describe Business, type: :model do
   it 'is valid with valid attributes' do
     expect(FactoryBot.build(:business)).to be_valid
